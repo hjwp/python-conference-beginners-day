3bf1462bc3157261745d3f54d12b4531aa7c4ab0
Harry <hjwp2@cantab.net>
2015-07-17 16:23:45 +0100

    extend magic methods challenge with moshes polynomial example

--- challenges/magic_methods.py
+++ challenges/magic_methods.py
@@ -33,3 +33,48 @@ class CardDeck:
             for rank in self.ranks
         ]
 
+
+
+"""
+Bonus exercise: Polynomial class
+
+Create a class that represents polynomials.  You may need to stretch your memory back to high school maths!
+
+A polynomial loks like
+
+    2(xx) - x + 7
+
+And its essential features are the coefficients of each power of x
+
+in this example, power-2=2, power-1=-1, power-0=7
+
+Credit to Moshe Goldstein
+"""
+
+class Polynomial:
+    def __init__(self, coefficients):
+        pass  # TODO
+
+    def __str__(self):
+        pass  # TODO
+
+    def __add__(self, poly):
+        '''returns the result of adding poly from self'''
+        pass  # TODO
+
+    def __sub__(self, poly):
+        '''returns the result of subtracting poly from self'''
+        pass  # TODO
+
+    def __mul__(self, poly):
+        '''multiply two polynomials'''
+        pass  # TODO
+
+    def value(self, x):
+        '''returns the value of the polynomial at point x'''
+        pass  # TODO
+
+    def derivative(self):
+        '''returns the derivate of the polynomial'''
+        pass  # TODO
+
